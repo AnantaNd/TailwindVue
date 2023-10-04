0f49cf69cc015b1bd0a823745d4d0d624cc5d6c2
AnantaNd <ananta.nov2@gmail.com>
2023-10-04 11:40:33 +0700

    fixing layout desktop

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,31 +3,40 @@
     <!-- hero -->
     <div class="min-h-screen">
       <div class="flex flex-col items-center">
-        <div class="absolute text-justify mt-20">
-          <h1 class="font-bold text-4xl">SITUS DIBLOKIR</h1>
-          <h2 class="font-bold text-xl italic">WEBSITE BLOCKED</h2><br/>
-          <p class="font-semibold text-sm">UNDANG-UNDANG NOMOR 19 TAHUN 2016 PASAL 40 (2a) dan (2b) TENTANG INFORMASI DAN TRANSAKSI ELEKTRONIK <br/><span class="italic font-light">Indonesia Law No.19 Year 2016 Article 40 (2a) and (2b) regarding Information and Electonic Transaction</span></p><br/>
-          <p class="font-semibold text-sm">PERATURAN MENTERI KOMUNIKASI DAN INFORMATIKA NO.5 TAHUN 2020 TENTANG PENYELENGGARA SISTEM ELEKTRONIK LINGKUP PRIVAT <br/><span class="italic font-light">Regulation of The Minister of Communications and Informatics of The Republic of Indonesia Number 5 of 2020 on Private Electronic System Operators</span></p><br/>
-          <h1 class="text-2xl">MAAF, AKSES KE SITUS INI DIBLOKIR OLEH PEMERINTAH INDONESIA <br/>KARENA MENGANDUNG KONTEN NEGATIF <br/>YANG MELANGGAR PERATURAN PERUNDANGAN DI INDONESIA</h1>
+        <div class="absolute text-justify mt-24 z-10 w-6/12 md:container  ">
+          <h1 class="font-bold text-4xl text-white">SITUS DIBLOKIR</h1>
+          <h2 class="font-medium text-xl italic text-white">WEBSITE BLOCKED</h2><br/>
+          <p class="font-semibold text-sm text-white">UNDANG-UNDANG NOMOR 19 TAHUN 2016 PASAL 40 (2a) dan (2b) TENTANG INFORMASI DAN TRANSAKSI ELEKTRONIK <br/><span class="italic font-light">Indonesia Law No.19 Year 2016 Article 40 (2a) and (2b) regarding Information and Electonic Transaction</span></p><br/>
+          <p class="font-semibold text-sm text-white">PERATURAN MENTERI KOMUNIKASI DAN INFORMATIKA NO.5 TAHUN 2020 TENTANG PENYELENGGARA SISTEM ELEKTRONIK LINGKUP PRIVAT <br/><span class="italic font-light">Regulation of The Minister of Communications and Informatics of The Republic of Indonesia Number 5 of 2020 on Private Electronic System Operators</span></p><br/>
+          <h1 class="text-xl text-white">MAAF, AKSES KE SITUS INI DIBLOKIR OLEH PEMERINTAH INDONESIA <br/>KARENA MENGANDUNG KONTEN NEGATIF <br/>YANG MELANGGAR PERATURAN PERUNDANGAN DI INDONESIA</h1>
         </div>
       </div>
-      <div>
-        <img class="relative h-screen bg-cover z-0" src="../assets/shape/AksesDiblokir-Dekstop-05.png"/>
+      <div class="min-h-screen" style="background: rgb(192,0,0) radial-gradient(circle, rgba(192,0,0,0.633578431372549) 0%, rgba(110,0,0,0.7344187675070029) 66%);">
+        <img class="relative h-screen bg-cover z-0 object-contain" src="../assets/shape/AksesDiblokir-Dekstop-05.png" style="filter: grayscale(100%);"/>
       </div>
       <!-- hero -->
     </div>
     <div class="container mx-auto px-4">
       <div class="my-0 mx-auto h-full w-full">
-        <div class="">
-          <img class="absolute top-0 left-0 m-4 z-2 w-20 h-20" src="../assets/logo/logo-01.png"/>
+        <!-- desktop -->
+        <div class="hidden md:hidden sm:hidden lg:flex">
+          <img class="absolute top-0 left-0 m-4 z-2 h-20" src="../assets/logo/logo-01.png"/>
           <img class="absolute h-full w-3/4 top-0 left-0 z-0 sm:hidden" src="../assets/shape/AksesDiblokir-Dekstop-02.png"/>
         </div>
-        <div>
+        <div class="hidden md:hidden sm:hidden lg:flex">
           <img class="absolute top-0 right-0 m-4 z-2 w-20 h-16" src="../assets/logo/logo-03.png"/>
           <img class="absolute w-1/2 top-0 right-0 z-0" src="../assets/shape/AksesDiblokir-Dekstop-03.png"/>
         </div>
+        <!-- mobile -->
+        <div class="flex lg:hidden">
+          <img class="absolute top-0 left-0 m-4 z-2 h-20" src="../assets/logo/logo-01.png"/>
+          <img class="absolute top-0 right-0 m-4 z-10 h-12" src="../assets/logo/logo-03.png"/>
+          <img class="absolute top-0 left-0 z-3 w-10" src="../assets/shape/MobileVersion-04.png"/>
+          <img class="absolute top-0 right-0 z-2 w-1/2" src="../assets/shape/MobileVersion-05.png"/>
+        </div>
         <div class="">
           <img class="w-1/2 absolute bottom-0 right-0 z-0" src="../assets/shape/AksesDiblokir-Dekstop-04.png"/>
+          <img class="absolute bottom-0 right-0 m-4 z-2 object-contain h-12" src="../assets/logo/logo-02.png"/>
         </div>
       </div>
     </div>

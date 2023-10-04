caaeaf750cb8f3c032aa2c12a76bc33e7e33014c
AnantaNd <ananta.nov2@gmail.com>
2023-10-04 22:15:51 +0700

    fixing responsive

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,44 +1,70 @@
 <template >
-  <div>
+  <div class="h-full md:overflow-y-auto" style="background: rgb(192,0,0) radial-gradient(circle, rgba(192,0,0,0.633578431372549) 0%, rgba(110,0,0,0.7344187675070029) 66%);height: 100%;">
     <!-- hero -->
     <div class="min-h-screen">
       <div class="flex flex-col items-center">
-        <div class="absolute text-justify mt-24 z-10 w-6/12 md:container  ">
-          <h1 class="font-bold text-4xl text-white">SITUS DIBLOKIR</h1>
+        <div class="absolute text-justify z-10 p-8 md:w-8/12" style="top: 50%; transform: translateY(-50%);">
+          <h1 class="font-bold text-2xl text-white">SITUS DIBLOKIR</h1>
           <h2 class="font-medium text-xl italic text-white">WEBSITE BLOCKED</h2><br/>
           <p class="font-semibold text-sm text-white">UNDANG-UNDANG NOMOR 19 TAHUN 2016 PASAL 40 (2a) dan (2b) TENTANG INFORMASI DAN TRANSAKSI ELEKTRONIK <br/><span class="italic font-light">Indonesia Law No.19 Year 2016 Article 40 (2a) and (2b) regarding Information and Electonic Transaction</span></p><br/>
           <p class="font-semibold text-sm text-white">PERATURAN MENTERI KOMUNIKASI DAN INFORMATIKA NO.5 TAHUN 2020 TENTANG PENYELENGGARA SISTEM ELEKTRONIK LINGKUP PRIVAT <br/><span class="italic font-light">Regulation of The Minister of Communications and Informatics of The Republic of Indonesia Number 5 of 2020 on Private Electronic System Operators</span></p><br/>
-          <h1 class="text-xl text-white">MAAF, AKSES KE SITUS INI DIBLOKIR OLEH PEMERINTAH INDONESIA <br/>KARENA MENGANDUNG KONTEN NEGATIF <br/>YANG MELANGGAR PERATURAN PERUNDANGAN DI INDONESIA</h1>
+          <p class="text-sm font-semibold text-white">MAAF, AKSES KE SITUS INI DIBLOKIR OLEH PEMERINTAH INDONESIA <br/>KARENA MENGANDUNG KONTEN NEGATIF <br/>YANG MELANGGAR PERATURAN PERUNDANGAN DI INDONESIA</p><br/>
+          <p class="text-sm font-semibold text-white">Sorry, access to this website has been denied by <br/>The Government of Indonesia due to negative<br/>content as it violates Indonesia Law</p>
         </div>
       </div>
-      <div class="min-h-screen" style="background: rgb(192,0,0) radial-gradient(circle, rgba(192,0,0,0.633578431372549) 0%, rgba(110,0,0,0.7344187675070029) 66%);">
+      <div class="min-h-screen">
         <img class="relative h-screen bg-cover z-0 object-contain" src="../assets/shape/AksesDiblokir-Dekstop-05.png" style="filter: grayscale(100%);"/>
       </div>
       <!-- hero -->
     </div>
-    <div class="container mx-auto px-4">
-      <div class="my-0 mx-auto h-full w-full">
-        <!-- desktop -->
-        <div class="hidden md:hidden sm:hidden lg:flex">
-          <img class="absolute top-0 left-0 m-4 z-2 h-20" src="../assets/logo/logo-01.png"/>
-          <img class="absolute h-full w-3/4 top-0 left-0 z-0 sm:hidden" src="../assets/shape/AksesDiblokir-Dekstop-02.png"/>
-        </div>
-        <div class="hidden md:hidden sm:hidden lg:flex">
-          <img class="absolute top-0 right-0 m-4 z-2 w-20 h-16" src="../assets/logo/logo-03.png"/>
-          <img class="absolute w-1/2 top-0 right-0 z-0" src="../assets/shape/AksesDiblokir-Dekstop-03.png"/>
-        </div>
-        <!-- mobile -->
-        <div class="flex lg:hidden">
-          <img class="absolute top-0 left-0 m-4 z-2 h-20" src="../assets/logo/logo-01.png"/>
-          <img class="absolute top-0 right-0 m-4 z-10 h-12" src="../assets/logo/logo-03.png"/>
-          <img class="absolute top-0 left-0 z-3 w-10" src="../assets/shape/MobileVersion-04.png"/>
-          <img class="absolute top-0 right-0 z-2 w-1/2" src="../assets/shape/MobileVersion-05.png"/>
-        </div>
-        <div class="">
-          <img class="w-1/2 absolute bottom-0 right-0 z-0" src="../assets/shape/AksesDiblokir-Dekstop-04.png"/>
-          <img class="absolute bottom-0 right-0 m-4 z-2 object-contain h-12" src="../assets/logo/logo-02.png"/>
-        </div>
-      </div>
+    <!-- desktop -->
+    <div class="container hidden md:flex sm:hidden lg:flex">
+      <div class="absolute top-0 left-0 bg-white min-h-screen z-3" 
+          style="
+            width: 768px;
+            clip-path: polygon(0% 0%, 22.5% 0%, 0% 96%);
+            filter: drop-shadow(12px 8px 8px #000000);
+            overflow:hidden;"></div>
+      <img class="absolute top-0 left-0 z-10" style="margin: 10px 1rem; width: 100px; height: 100px;" src="../assets/logo/logo-01.png"/>
+      <div class="absolute top-0 right-0 h-full" 
+          style="
+            width: 750px; 
+            clip-path: polygon(34.4% 0%, 100.1% 0%, 100.1% 25.5%);
+            background: linear-gradient(90deg, #474bff 0%, #00057e 100%);
+            filter: drop-shadow(-13px 9px 8px #000000);"></div>
+      <img class="absolute top-0 right-0 m-4 z-2 w-20 h-16" src="../assets/logo/logo-03.png"/>
+      <div class="absolute bottom-0 right-0" 
+          style="
+            height: 550px;
+            width: 800px; 
+            clip-path: polygon(25.1% 100.3%, 100.2% 100.3%, 100.2% 74.8%); 
+            background: linear-gradient(90deg, #474bff 0%, #00057e 100%);"></div>
+      <h1 class="absolute bottom-0 right-0 z-2 text-white font-bold text-xl">www.varnion.com</h1>
+    </div>
+    <!-- mobile -->
+    <div class="flex lg:hidden md:hidden">
+      <div class="absolute top-0 left-0 bg-white h-350-px z-3 drop-shadow" 
+          style="
+            width: 500px;
+            height: 480px;
+            clip-path: polygon(50% 0%, 0% 25.8%, 0% 0%);
+            filter: drop-shadow(12px 8px 8px #000000);"></div>
+      <img class="absolute top-0 left-0 z-10" style="margin: 0px 1rem; width: 80px; height: 80px;" src="../assets/logo/logo-01.png"/>
+      <div class="absolute top-0 left-0 z-2 drop-shadow" 
+          style="
+            width: 100%;
+            height: 480px;
+            clip-path: polygon(50% 0%, 100.1% 0%, 100.1% 21.5%);
+            background: linear-gradient(90deg, #474bff 0%, #00057e 100%);
+            filter: drop-shadow(12px 8px 8px #000000);"></div>
+      <img class="absolute top-0 right-0 m-1 z-2 object-contain" style="width: 80px; height: 80px;" src="../assets/logo/logo-03.png"/>
+      <div class="absolute bottom-0 left-0 h-full z-3 drop-shadow" 
+          style="
+            width: 100%;
+            height: 400px;
+            clip-path: polygon(25.1% 100.3%, 100.2% 100.3%, 100.2% 74.8%); 
+            background: linear-gradient(90deg, #474bff 0%, #00057e 100%);
+            filter: drop-shadow(12px 8px 8px #000000);"></div>
     </div>
   </div>
 </template>
